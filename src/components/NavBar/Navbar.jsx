d4ea606203c6ab984fdb786b12a17935d86d5310
Vishal <vishal.ranjan7777@gmail.com>
2024-02-08 21:56:53 -0600

    Fix formatting issue

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -18,24 +18,24 @@ export const Navbar = () => {
                     alt="menu-button"
                     onClick={() => setIsMenuOpen(!isMenuOpen)}
                 />
-            <ul
-                className={`${styles.menuItems} ${isMenuOpen && styles.menuOpen}`}
-                onClick={() => setIsMenuOpen(false)}
-            >
-            <li>
-                <a href="#about"> About </a>
-            </li>
-            <li>
-                <a href="#experience"> Experience </a>
-            </li>
-            <li>
-                <a href="#projects"> Projects </a>
-            </li>
-            <li>
-                <a href="#contact"> Contact </a>
-            </li>
-            </ul>
-        </div>
+                <ul
+                    className={`${styles.menuItems} ${isMenuOpen && styles.menuOpen}`}
+                    onClick={() => setIsMenuOpen(false)}
+                >
+                <li>
+                    <a href="#about"> About </a>
+                </li>
+                <li>
+                    <a href="#experience"> Experience </a>
+                </li>
+                <li>
+                    <a href="#projects"> Projects </a>
+                </li>
+                <li>
+                    <a href="#contact"> Contact </a>
+                </li>
+                </ul>
+            </div>
         </nav>
     );
 };
