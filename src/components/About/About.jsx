58e43c59e98d4b5f8797d0b105e33a4f393d23fe
Vishal <vishal.ranjan7777@gmail.com>
2024-05-14 00:05:12 -0500

    Improve text statements

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,31 +17,35 @@ export const About = () => {
                     <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
                         <div className={styles.aboutItemText}>
-                        <h3>Frontend Developer</h3>
-                        <p>
-                            I'm a frontend developer with experience in building responsive
-                            and optimized sites
-                        </p>
+                            <h3>Frontend Developer</h3>
+                            <p>
+                                As a professional frontend developer, I possess the necessary skills and expertise to design and develop responsive and optimized websites.
+                                My experience in this field has equipped me with a comprehensive understanding of what it takes to create a website
+                                that is both user-friendly and aesthetically appealing.
+                                I am confident that I can leverage my skills and knowledge to deliver a website that meets your expectations.
+                            </p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
                         <div className={styles.aboutItemText}>
-                        <h3>Backend Developer</h3>
-                        <p>
-                            I have experience developing fast and optimised back-end systems
-                            and APIs
-                        </p>
+                            <h3>Backend Developer</h3>
+                            <p>
+                                I possess extensive expertise in developing highly efficient and optimized back-end systems and APIs
+                                that can significantly enhance the organization's technological capabilities.
+                                With my skill set, I am confident that I can deliver exceptional results and help stay ahead of the curve.
+                            </p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
                         <div className={styles.aboutItemText}>
-                        <h3>UI Designer</h3>
-                        <p>
-                            I have designed multiple landing pages and have created design
-                            systems as well
-                        </p>
+                            <h3>UI Designer</h3>
+                            <p>
+                            I have vast experience designing landing pages and developing design systems.
+                            Let me help you create an aesthetically pleasing and user-friendly landing page that aligns with your website goals.
+                            I am confident that my attention to detail and design principles will exceed your expectations.
+                            </p>
                         </div>
                     </li>
                 </ul>
