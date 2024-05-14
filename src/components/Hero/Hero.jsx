5302e7a4c56fea1327d9951afe8f87f7b764dc03
Vishal <vishal.ranjan7777@gmail.com>
2024-05-13 23:42:07 -0500

    Add project pictures and add resume and change some text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,7 +18,7 @@ export const Hero = () => {
             </a>
         </div>
         <img
-            src={getImageUrl('hero/heroImage.png')}
+            src={getImageUrl('hero/hero-portfolio.png')}
             alt="Hero image of me"
             className={styles.heroImg}
         />
